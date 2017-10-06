8d565af5069691a22c3dc197a0990b66a934dbde
Dolav Nitay <dolavn16@gmail.com>
2017-10-06 18:48:06 +0300

    test

--- test.php
+++ test.php
@@ -2,7 +2,17 @@
 <head></head>
 <body>
 <?php
-	phpinfo();
+	class MyDB extends SQLite3 {
+		function __construct(){
+			$this->open('data.db');
+		}
+	}
+	$db = new MyDB();
+	if(!$db){
+		echo $db->lastErrorMsg();
+	}else{
+		echo "Database opened successfully\n";
+	}
 ?>
 </body>
 </html>
