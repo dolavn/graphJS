96c5136f86fa2599db4854edacf77c13126170c3
Dolav Nitay <dolavn16@gmail.com>
2017-10-07 01:30:51 +0300

    first php

--- createDB.php
+++ createDB.php
@@ -1,7 +1,17 @@
 <?php
 	include('dbconn.php');
 	$db = new Database();
-	#$str = "CREATE TABLE members(member_id INT PRIMARY KEY,user_name TEXT NOT NULL)";
-	$str = "INSERT INTO members(member_id,user_name) VALUES (2,'Tal')";
+	$str = "DROP TABLE members";
+	#$db->exec($str);
+	$str = "CREATE TABLE members(
+	member_id INT PRIMARY KEY,
+	user_name TEXT NOT NULL,
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT NOT NULL,
+	password VARCHAR(50) NOT NULL)";
+	$db->exec($str);
+	$str = "INSERT INTO members(member_id,user_name,first_name,last_name,email,password) 
+	VALUES (1,'dolavn','Dolav','Nitay','dolavn@post.bgu.ac.il','c129b324aee662b04eccf68babba85851346dff9')";
 	$db->exec($str);
 ?>
